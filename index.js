18d84cbe8a01a280c6dd8a6d8c857e5e8ea40c52
whroman <me@walterroman.com>
2015-12-18 00:08:20 +0100

    Init. Turn is in place.

---
+++ index.js
@@ -0,0 +1,1 @@
+require("./stores/Turn.js");
---
+++ stores/Turn.spec.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const assert = require("chai").assert;
+const Turn = require("./Turn.js");
+
+describe("Turn", () => {
+    describe(".getRemainingPins()", () => {
+        const testCases = [1,10,100];
+
+        it("should return all pins immediately after init", () => {
+            testCases.forEach((numOfPins) => {
+                const turn = new Turn(2, numOfPins);
+                const remainingPins = turn.getRemainingPins();
+                assert.equal(remainingPins.length, numOfPins);
+            });
+        });
+
+        it("should return no pins if all are knocked down", () => {
+            testCases.forEach((numOfPins) => {
+                const turn = new Turn(2, numOfPins);
+                let key;
+                for (key in turn.pins) {
+                    turn.pins[key] = true;
+                }
+                const remainingPins = turn.getRemainingPins();
+                assert.equal(remainingPins.length, 0);
+            });
+        });
+    });
+
+    describe(".roll()", () => {
+        const testCases = [];
+
+        const addCase = function (pinsToAffect, numOfPins) {
+            testCases.push({
+                pinsToAffect: pinsToAffect,
+                numOfPins: numOfPins,
+                expect: numOfPins - pinsToAffect.length
+            });
+        };
+
+        addCase([1,2,3], 10);
+        addCase([1,2,3,6,7], 100);
+
+        it("should knock down all given pins by ID", () => {
+            testCases.forEach((testCase) => {
+                const turn = new Turn(2, testCase.numOfPins);
+                turn.roll(testCase.pinsToAffect);
+                assert.equal(
+                    turn.getRemainingPins().length,
+                    testCase.numOfPins - testCase.pinsToAffect.length
+                );
+
+            });
+        });
+    });
+});
